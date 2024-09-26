6f5b921414a632e401f32a4c31bfa7ac83513624
Sebastian Weyrauch <github@dev-lab.de>
2024-09-26 09:31:13 +0200

    fix: added missing import statement

--- extensions/CleverPushNotificationContentExtension/NotificationViewController.swift
+++ extensions/CleverPushNotificationContentExtension/NotificationViewController.swift
@@ -1,6 +1,7 @@
 import UIKit
 import UserNotifications
 import UserNotificationsUI
+import CleverPush
 
 class NotificationViewController: CPNotificationViewController, UNNotificationContentExtension {
 
