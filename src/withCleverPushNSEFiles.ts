d0befa27d2a4f6f3c22c918e61792a0407492ee9
Sebastian Weyrauch <github@dev-lab.de>
2024-09-26 05:02:04 +0200

    feat: replacing of version/build number in Info.plist

--- src/withCleverPushNSEFiles.ts
+++ src/withCleverPushNSEFiles.ts
@@ -17,13 +17,22 @@ export const withCleverPushNSEFiles: ConfigPlugin<CleverPushPluginProps> = (conf
 		'ios',
 		async (config) => {
 			const iosPath = path.join(config.modRequest.projectRoot, 'ios');
+			const nsePath = path.join(iosPath, NSE_TARGET_NAME);
 
-			await fs.promises.mkdir(`${iosPath}/${NSE_TARGET_NAME}`, { recursive: true });
+			await fs.promises.mkdir(nsePath, { recursive: true });
 
 			await Promise.all([NSE_FILES.map(async (file) => {
-				const targetFile = `${iosPath}/${NSE_TARGET_NAME}/${file}`;
+				const targetFile = `${nsePath}/${file}`;
 
 				await fs.promises.copyFile(`${sourcePath}/${file}`, targetFile);
+
+				// The version and build number in the Info.plist need to match the version of the main target
+				if (targetFile === 'Info.plist') {
+					let content = await fs.promises.readFile(`${nsePath}/Info.plist`, 'utf8');
+					content = content.replace(/(<key>CFBundleShortVersionString<\/key>\s+<string>)(\d.+)(<\/string>)/gm, `$1${config?.version}$3`);
+					content = content.replace(/(<key>CFBundleVersion<\/key>\s+<string>)(\d+)(<\/string>)/gm, `$1${config.ios?.buildNumber}$3`);
+					await fs.promises.writeFile(`${nsePath}/Info.plist`, content);
+				}
 			})]);
 
 			return config;
